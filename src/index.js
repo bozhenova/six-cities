582385d86eb45f64fe8e996a4d6c6bae0798ebeb
Margo <margo.bozhenova@gmail.com>
2020-06-17 01:41:45 +0300

    Add Header component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/app';
 
-ReactDOM.render(<App />, document.getElementById(`root`));
+ReactDOM.render(
+  <App
+    names={[
+      `Beautiful & luxurious apartment at great location`,
+      `Wood and stone place`,
+      `Canal View Prinsengracht`,
+      `Nice, cozy, warm big bed apartment`
+    ]}
+  />,
+  document.getElementById(`root`)
+);
