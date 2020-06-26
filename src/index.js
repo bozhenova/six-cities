d3d04664adc9f179d58ccc5bdd68d046f7f3da69
Margo <margo.bozhenova@gmail.com>
2020-06-27 00:07:11 +0300

    Add ErrorBoundary and ErrorIndicator components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reducer from './redux/reducer';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-import reducer from './redux/reducer';
+import { HashRouter as Router } from 'react-router-dom';
+
+import ErrorBoundary from './components/error-boundary/error-boundary';
 import App from './components/app/app';
 import OfferDetails from './components/offer-details/offer-details';
-import offers from './mocks/offers';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App offers={offers} />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Router>
+          <App />
+        </Router>
+      </BrowserRouter>
+    </ErrorBoundary>
   </Provider>,
   document.getElementById(`root`)
 );
