a7df86cfe3383d47c7c91eddccf8404aff710d38
Margo <margo.bozhenova@gmail.com>
2020-06-29 02:03:44 +0300

    Add id to mock data

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -1,5 +1,6 @@
 const offers = [
   {
+    id: 423324,
     city: `Amsterdam`,
     title: `Nice, cozy, warm big bed apartment`,
     previewPhoto: `img/apartment-03.jpg`,
@@ -11,6 +12,7 @@ const offers = [
     coordinates: [52.3809553943508, 4.939309666406198]
   },
   {
+    id: 4224,
     city: `Amsterdam`,
     title: `Beautiful & luxurious apartment at great location`,
     previewPhoto: `img/apartment-01.jpg`,
@@ -22,6 +24,7 @@ const offers = [
     coordinates: [52.3909553943508, 4.85309666406198]
   },
   {
+    id: 42334224,
     city: `Amsterdam`,
     title: `Wood and stone place`,
     previewPhoto: `img/room.jpg`,
@@ -33,15 +36,64 @@ const offers = [
     coordinates: [52.369553943508, 4.85309666406198]
   },
   {
-    city: `Amsterdam`,
+    id: 4233924,
+    city: `Brussels`,
     title: `Canal View Prinsengracht`,
     previewPhoto: `img/apartment-02.jpg`,
-    isPremium: true,
+    isPremium: false,
     isFavorite: false,
     price: 132,
     type: `Apartment`,
     rating: 80,
     coordinates: [52.3909553943508, 4.929309666406198]
+  },
+  {
+    id: 4230324,
+    city: `Paris`,
+    title: `Beautiful & luxurious apartment at great location`,
+    previewPhoto: `img/apartment-01.jpg`,
+    isPremium: true,
+    isFavorite: false,
+    price: 120,
+    type: `Apartment`,
+    rating: 93,
+    coordinates: [48.86679, 2.37852]
+  },
+  {
+    id: 42317324,
+    city: `Cologne`,
+    title: `Wood and stone place`,
+    previewPhoto: `img/room.jpg`,
+    isPremium: false,
+    isFavorite: true,
+    price: 80,
+    type: `Private room`,
+    rating: 80,
+    coordinates: [50.958694, 6.961092]
+  },
+  {
+    id: 4233290,
+    city: `Hamburg`,
+    title: `Canal View Prinsengracht`,
+    previewPhoto: `img/apartment-02.jpg`,
+    isPremium: false,
+    isFavorite: false,
+    price: 132,
+    type: `Apartment`,
+    rating: 80,
+    coordinates: [53.598154, 10.062009]
+  },
+  {
+    id: 423487324,
+    city: `Dusseldorf`,
+    title: `Nice, cozy, warm big bed apartment`,
+    previewPhoto: `img/apartment-03.jpg`,
+    isPremium: false,
+    isFavorite: false,
+    price: 180,
+    type: `Apartment`,
+    rating: 80,
+    coordinates: [51.261306, 6.804574]
   }
 ];
 
