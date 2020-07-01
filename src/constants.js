b8d15b81b8708e23ff526e38f7aab7afbddc3624
Margo <margo.bozhenova@gmail.com>
2020-07-02 02:36:32 +0300

    Add constants

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,26 @@
 export const CITIES = new Map([
-  [`Paris`, [48.856613, 2.352222]],
-  [`Cologne`, [50.937531, 6.960279]],
-  [`Brussels`, [50.850346, 4.351721]],
-  [`Amsterdam`, [52.370216, 4.895168]],
-  [`Hamburg`, [53.551086, 9.993682]],
-  [`Dusseldorf`, [51.227741, 6.773456]]
+  ['Amsterdam', [52.370216, 4.895168]],
+  ['Paris', [48.856613, 2.352222]],
+  ['Cologne', [50.937531, 6.960279]],
+  ['Brussels', [50.850346, 4.351721]],
+  ['Hamburg', [53.551086, 9.993682]],
+  ['Dusseldorf', [51.227741, 6.773456]]
 ]);
+
+export const Constants = {
+  CITIES: [
+    'Amsterdam',
+    'Paris',
+    'Cologne',
+    'Brussels',
+    'Hamburg',
+    'Dusseldorf'
+  ],
+  DEFAULT_CITY: 'Amsterdam',
+  BASE_URL: `https://es31-server.appspot.com/six-cities`,
+  HOTEL_PATH: `/hotels`,
+  ACCESS_DENIED: 403,
+  STATUS_OK: 200,
+  TIMEOUT: 5000,
+  MAX_RATING: 5
+};
