45b2caf8748c698d6950bebcd331a7eb5e7ddad9
Margo <margo.bozhenova@gmail.com>
2020-07-06 03:16:47 +0300

    Render 3 nearby offers and active offer on the map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { CITIES } from '../../../src/constants';
 import leaflet from 'leaflet';
+import { connect } from 'react-redux';
 
 const MapConfig = {
   ID: `map`,
-  ZOOM: 12,
   MARKER_PATH: `img/pin.svg`,
   ACTIVE_MARKER_PATH: `img/pin-active.svg`,
   MARKER_SIZE: [30, 30],
   TITLE_LAYER: `https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`
 };
 
-class Map extends Component {
+class Map extends PureComponent {
   static propTypes = {
-    coordinates: PropTypes.array.isRequired,
-    currentCity: PropTypes.string.isRequired,
-    selectedOffer: PropTypes.array
+    offers: PropTypes.array.isRequired,
+    filteredOffers: PropTypes.array.isRequired,
+    currentOfferId: PropTypes.number
   };
 
   static defaultProps = {
-    selectedOffer: []
+    currentOfferId: null,
+    offers: []
   };
 
+  coordinates = this.props.offers.map(offer => offer.place.coords);
+  city = this.props.offers[0].city;
   group = null;
   icon = leaflet.icon({
     iconUrl: MapConfig.MARKER_PATH,
@@ -33,28 +35,33 @@ class Map extends Component {
     iconSize: MapConfig.MARKER_SIZE
   });
 
+  getCurrentOffer = () => {
+    const { currentOfferId, filteredOffers } = this.props;
+    const currentOffer = filteredOffers.find(
+      item => item.id === currentOfferId
+    );
+    return currentOffer ? currentOffer.place.coords : [0, 0];
+  };
+
   addMarkers() {
-    const { selectedOffer, coordinates } = this.props;
-
     this.group = leaflet.layerGroup().addTo(this.map);
-
-    for (let item of coordinates) {
+    for (let item of this.coordinates) {
       leaflet.marker(item, { icon: this.icon }).addTo(this.group);
     }
-    leaflet.marker(selectedOffer, { icon: this.activeIcon }).addTo(this.group);
+    leaflet
+      .marker(this.getCurrentOffer(), { icon: this.activeIcon })
+      .addTo(this.group);
   }
 
   initMap() {
-    const city = CITIES.get(this.props.currentCity);
-
     this.map = leaflet.map(MapConfig.ID, {
-      center: city,
-      zoom: MapConfig.ZOOM,
+      center: this.city.coords,
+      zoom: this.city.zoom,
       zoomControl: false,
       marker: true,
       scrollWheelZoom: false
     });
-    this.map.setView(city, MapConfig.ZOOM);
+    this.map.setView(this.city.coords, this.city.zoom);
 
     leaflet.tileLayer(MapConfig.TITLE_LAYER).addTo(this.map);
     this.addMarkers();
@@ -65,16 +72,19 @@ class Map extends Component {
   }
 
   componentDidUpdate() {
-    const { currentCity, coordinates, selectedOffer } = this.props;
+    this.city = this.props.offers[0].city;
+    this.coordinates = this.props.offers.map(offer => offer.place.coords);
 
     this.group.clearLayers();
-    this.map.setView(CITIES.get(currentCity), MapConfig.ZOOM);
+    this.map.setView(this.city.coords, this.city.zoom);
 
-    for (let item of coordinates) {
+    for (let item of this.coordinates) {
       leaflet.marker(item, { icon: this.icon }).addTo(this.group);
     }
 
-    leaflet.marker(selectedOffer, { icon: this.activeIcon }).addTo(this.group);
+    leaflet
+      .marker(this.getCurrentOffer(), { icon: this.activeIcon })
+      .addTo(this.group);
   }
 
   render() {
@@ -88,4 +98,10 @@ class Map extends Component {
   }
 }
 
-export default Map;
+const mapStateToProps = state => ({
+  currentOfferId: state.data.currentOfferId,
+  filteredOffers: state.data.filteredOffers
+});
+
+export { Map };
+export default connect(mapStateToProps)(Map);
