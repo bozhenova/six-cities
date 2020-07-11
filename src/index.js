d60faffa122d1fd068e2f2a94b4ebc9fb70102d5
Margo <margo.bozhenova@gmail.com>
2020-07-12 00:39:15 +0300

    Replace BrowserRouter with Router, add constants

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import { compose } from 'recompose';
+import history from './history';
 
 import App from './components/app';
 import configureAPI from './services/api';
@@ -14,7 +15,7 @@ import { Operations as UserOperations } from './redux/reducer/user/actions';
 import { Operations as DataOperations } from './redux/reducer/data/actions';
 
 const initApp = () => {
-  const api = configureAPI((...args) => store.dispatch(...args));
+  const api = configureAPI(() => history.push('/login'));
   const store = createStore(
     rootReducer,
     compose(
@@ -30,7 +31,7 @@ const initApp = () => {
   ReactDOM.render(
     <Provider store={store}>
       <ErrorBoundary>
-        <Router>
+        <Router history={history}>
           <App />
         </Router>
       </ErrorBoundary>
