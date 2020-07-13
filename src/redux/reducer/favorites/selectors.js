c34d6168dc3cb7df4e3e9ce26a646ed194786d7a
Margo <margo.bozhenova@gmail.com>
2020-07-14 01:29:16 +0300

    Add groupFavoritesByCities function

--- src/redux/reducer/favorites/selectors.js
+++ src/redux/reducer/favorites/selectors.js
@@ -1,3 +1,20 @@
-import { createSelector } from 'reselect';
-
 export const getFavorites = state => state.favorites.favorites;
+
+export const groupFavoritesByCities = state => {
+  const favorites = getFavorites(state);
+
+  if (!favorites.length) {
+    return false;
+  }
+  const citiesKeys = [...new Set([...favorites.map(item => item.city.name)])];
+  const group = {};
+
+  citiesKeys.forEach(item => {
+    group[item] = [];
+  });
+  favorites.forEach(item => {
+    group[item.city.name].push(item);
+  });
+
+  return group;
+};
