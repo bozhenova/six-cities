4d865d1c4080e285f03b85f55f3128e2aa4e2278
Margo <margo.bozhenova@gmail.com>
2020-07-14 01:29:33 +0300

    Add updateItemsList, findItemById functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,13 @@ export const parseDate = date => {
   const options = { month: 'long', year: 'numeric' };
   return new Date(date).toLocaleDateString('en-US', options);
 };
+
+export const updateItemsList = (items, newItem) => {
+  return items.length
+    ? items.map(item => {
+        return item.id === newItem.id ? newItem : item;
+      })
+    : false;
+};
+
+export const findItemById = (id, items) => items.find(item => item.id === id);
