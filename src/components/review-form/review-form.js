1f68eacc4dacb6f5d306f724de513dc2fb294f26
Margo <margo.bozhenova@gmail.com>
2020-07-18 00:46:38 +0300

    Add hooks to ReviewForm instead of withReviewForm

--- src/components/review-form/review-form.js
+++ src/components/review-form/review-form.js
@@ -1,180 +1,189 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import * as selectors from '../../redux/reducer/reviews/selectors';
+import {
+  getReviewSendingStatus,
+  getReviewSendingProcessStatus
+} from '../../redux/reducer/reviews/selectors';
 import { ActionCreator, Operations } from '../../redux/reducer/reviews/actions';
-import withReviewForm from '../../hoc/with-review-form';
 import { Constants } from '../../constants';
 
-class ReviewForm extends Component {
-  form = React.createRef();
+const ReviewForm = ({ currentOfferId }) => {
+  const form = useRef();
+  const dispatch = useDispatch();
 
-  handleFormSubmit = e => {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const isSent = useSelector(getReviewSendingStatus);
+  const isSending = useSelector(getReviewSendingProcessStatus);
+
+  const handleTextareaChange = e => {
+    setComment(e.target.value);
+  };
+
+  const handleRatingChange = e => {
+    setRating(e.target.value);
+  };
+
+  const handleFormSubmit = e => {
     e.preventDefault();
-    const { postReview, rating, comment, currentOfferId } = this.props;
-    postReview(currentOfferId, { comment, rating });
+    dispatch(Operations.postReview(currentOfferId, { comment, rating }));
+    dispatch(ActionCreator.lockForm(true));
   };
 
-  handleRatingChange = e => {
-    this.props.onChangeRating(e.target.value);
-  };
-
-  componentDidUpdate() {
-    const { isSent, setSendingStatus } = this.props;
+  useEffect(() => {
+    const hasText =
+      comment.length >= Constants.MIN_TEXT_LENGTH &&
+      comment.length <= Constants.MAX_TEXT_LENGTH;
+    const hasRating = rating > 0;
+    setIsValid(hasText && hasRating);
 
     if (isSent) {
-      setSendingStatus();
-      this.form.current.reset();
+      setIsValid(false);
+      setRating(0);
+      setComment('');
+      dispatch(ActionCreator.setSendingStatus(false));
+      form.current.reset();
     }
-  }
+  }, [comment, rating, isSent]);
 
-  render() {
-    const { onChangeText, isFormValid, comment, isSending } = this.props;
-    return (
-      <form
-        className='reviews__form form'
-        action='#'
-        method='post'
-        onSubmit={this.handleFormSubmit}
-        ref={this.form}
+  return (
+    <form
+      className='reviews__form form'
+      action='#'
+      method='post'
+      onSubmit={handleFormSubmit}
+      ref={form}
+    >
+      <label className='reviews__label form__label' htmlFor='review'>
+        Your review
+      </label>
+      <div
+        className='reviews__rating-form form__rating'
+        onChange={handleRatingChange}
       >
-        <label className='reviews__label form__label' htmlFor='review'>
-          Your review
-        </label>
-        <div
-          className='reviews__rating-form form__rating'
-          onChange={this.handleRatingChange}
-        >
-          <input
-            className='form__rating-input visually-hidden'
-            name='rating'
-            value='5'
-            id='5-stars'
-            type='radio'
-          />
-          <label
-            htmlFor='5-stars'
-            className='reviews__rating-label form__rating-label'
-            title='perfect'
-          >
-            <svg className='form__star-image' width='37' height='33'>
-              <use xlinkHref='#icon-star' />
-            </svg>
-          </label>
-
-          <input
-            className='form__rating-input visually-hidden'
-            name='rating'
-            value='4'
-            id='4-stars'
-            type='radio'
-          />
-          <label
-            htmlFor='4-stars'
-            className='reviews__rating-label form__rating-label'
-            title='good'
-          >
-            <svg className='form__star-image' width='37' height='33'>
-              <use xlinkHref='#icon-star' />
-            </svg>
-          </label>
-
-          <input
-            className='form__rating-input visually-hidden'
-            name='rating'
-            value='3'
-            id='3-stars'
-            type='radio'
-          />
-          <label
-            htmlFor='3-stars'
-            className='reviews__rating-label form__rating-label'
-            title='not bad'
-          >
-            <svg className='form__star-image' width='37' height='33'>
-              <use xlinkHref='#icon-star' />
-            </svg>
-          </label>
-
-          <input
-            className='form__rating-input visually-hidden'
-            name='rating'
-            value='2'
-            id='2-stars'
-            type='radio'
-          />
-          <label
-            htmlFor='2-stars'
-            className='reviews__rating-label form__rating-label'
-            title='badly'
-          >
-            <svg className='form__star-image' width='37' height='33'>
-              <use xlinkHref='#icon-star' />
-            </svg>
-          </label>
-
-          <input
-            className='form__rating-input visually-hidden'
-            name='rating'
-            value='1'
-            id='1-star'
-            type='radio'
-          />
-          <label
-            htmlFor='1-star'
-            className='reviews__rating-label form__rating-label'
-            title='terribly'
-          >
-            <svg className='form__star-image' width='37' height='33'>
-              <use xlinkHref='#icon-star' />
-            </svg>
-          </label>
-        </div>
-        <textarea
-          className='reviews__textarea form__textarea'
-          id='review'
-          name='review'
-          placeholder='Tell how was your stay, what you like and what can be improved'
-          value={comment}
-          onChange={onChangeText}
-          minLength={Constants.MIN_TEXT_LENGTH}
-          maxLength={Constants.MAX_TEXT_LENGTH}
+        <input
+          className='form__rating-input visually-hidden'
+          name='rating'
+          value='5'
+          id='5-stars'
+          type='radio'
         />
-        <div className='reviews__button-wrapper'>
-          <p className='reviews__help'>
-            To submit review please make sure to set{' '}
-            <span className='reviews__star'>rating</span> and describe your stay
-            with at least{' '}
-            <b className='reviews__text-amount'>
-              {Constants.MIN_TEXT_LENGTH} characters
-            </b>
-            .
-          </p>
-          <button
-            className='reviews__submit form__submit button'
-            type='submit'
-            disabled={!isFormValid || isSending}
-          >
-            {isSending ? `Sending...` : `Submit`}
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+        <label
+          htmlFor='5-stars'
+          className='reviews__rating-label form__rating-label'
+          title='perfect'
+        >
+          <svg className='form__star-image' width='37' height='33'>
+            <use xlinkHref='#icon-star' />
+          </svg>
+        </label>
 
-const mapStateToProps = state => ({
-  isSent: selectors.getReviewSendingStatus(state),
-  isSending: selectors.getReviewSendingProcessStatus(state)
-});
+        <input
+          className='form__rating-input visually-hidden'
+          name='rating'
+          value='4'
+          id='4-stars'
+          type='radio'
+        />
+        <label
+          htmlFor='4-stars'
+          className='reviews__rating-label form__rating-label'
+          title='good'
+        >
+          <svg className='form__star-image' width='37' height='33'>
+            <use xlinkHref='#icon-star' />
+          </svg>
+        </label>
 
-const mapDispatchToProps = dispatch => ({
-  postReview: (id, review) => {
-    dispatch(Operations.postReview(id, review));
-    dispatch(ActionCreator.lockForm(true));
-  },
-  setSendingStatus: () => dispatch(ActionCreator.setSendingStatus(false))
-});
+        <input
+          className='form__rating-input visually-hidden'
+          name='rating'
+          value='3'
+          id='3-stars'
+          type='radio'
+        />
+        <label
+          htmlFor='3-stars'
+          className='reviews__rating-label form__rating-label'
+          title='not bad'
+        >
+          <svg className='form__star-image' width='37' height='33'>
+            <use xlinkHref='#icon-star' />
+          </svg>
+        </label>
 
-const ReviewFormWrapped = withReviewForm(ReviewForm);
-export default connect(mapStateToProps, mapDispatchToProps)(ReviewFormWrapped);
+        <input
+          className='form__rating-input visually-hidden'
+          name='rating'
+          value='2'
+          id='2-stars'
+          type='radio'
+        />
+        <label
+          htmlFor='2-stars'
+          className='reviews__rating-label form__rating-label'
+          title='badly'
+        >
+          <svg className='form__star-image' width='37' height='33'>
+            <use xlinkHref='#icon-star' />
+          </svg>
+        </label>
+
+        <input
+          className='form__rating-input visually-hidden'
+          name='rating'
+          value='1'
+          id='1-star'
+          type='radio'
+        />
+        <label
+          htmlFor='1-star'
+          className='reviews__rating-label form__rating-label'
+          title='terribly'
+        >
+          <svg className='form__star-image' width='37' height='33'>
+            <use xlinkHref='#icon-star' />
+          </svg>
+        </label>
+      </div>
+      <textarea
+        className='reviews__textarea form__textarea'
+        id='review'
+        name='review'
+        placeholder='Tell how was your stay, what you like and what can be improved'
+        value={comment}
+        onChange={handleTextareaChange}
+        minLength={Constants.MIN_TEXT_LENGTH}
+        maxLength={Constants.MAX_TEXT_LENGTH}
+      />
+      <div className='reviews__button-wrapper'>
+        <p className='reviews__help'>
+          To submit review please make sure to set{' '}
+          <span className='reviews__star'>rating</span> and describe your stay
+          with at least{' '}
+          <b className='reviews__text-amount'>
+            {Constants.MIN_TEXT_LENGTH} characters
+          </b>
+          .
+        </p>
+        <button
+          className='reviews__submit form__submit button'
+          type='submit'
+          disabled={!isValid || isSending}
+        >
+          {isSending ? `Sending...` : `Submit`}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+ReviewForm.propTypes = {
+  current: PropTypes.string.isRequired
+};
+
+export default ReviewForm;
