176c97a540c90cebf651e70a85d838b3441a3e3d
Margo <margo.bozhenova@gmail.com>
2020-07-19 00:26:10 +0300

    Add hooks to OffersList

--- src/components/offers-list/offers-list.js
+++ src/components/offers-list/offers-list.js
@@ -1,52 +1,43 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import CardWrapped from '../card';
-import { ActionCreator as DataActions } from '../../redux/reducer/data/actions';
+import { ActionCreator } from '../../redux/reducer/data/actions';
 
-class OffersList extends PureComponent {
-  static defaultProps = {
-    classModOffers: []
+const OffersList = ({
+  classModPrefix,
+  offers,
+  mainClassMod,
+  classModOffers
+}) => {
+  const dispatch = useDispatch();
+
+  const onOfferSelect = id => {
+    dispatch(ActionCreator.setCurrentOffer(id));
   };
 
-  static propTypes = {
-    offers: PropTypes.arrayOf(PropTypes.object),
-    setCurrentOffer: PropTypes.func.isRequired,
-    classModOffers: PropTypes.array,
-    classModPrefix: PropTypes.string.isRequired,
-    mainClassMod: PropTypes.string.isRequired
-  };
-
-  onOfferSelect = id => {
-    const { setCurrentOffer } = this.props;
-    setCurrentOffer(id);
-  };
-
-  render() {
-    const { classModPrefix, offers, mainClassMod, classModOffers } = this.props;
-    const places = offers.map(offer => {
-      return (
-        <CardWrapped
-          key={offer.id}
-          offerDetails={offer}
-          selectOffer={this.onOfferSelect}
-          classModPrefix={classModPrefix}
-          mainClassMod={mainClassMod}
-        />
-      );
-    });
+  const places = offers.map(offer => {
     return (
-      <div className={`${classModOffers.join(` `)} places__list `}>
-        {places}
-      </div>
+      <CardWrapped
+        key={offer.id}
+        offerDetails={offer}
+        selectOffer={onOfferSelect}
+        classModPrefix={classModPrefix}
+        mainClassMod={mainClassMod}
+      />
     );
-  }
-}
+  });
+  return (
+    <div className={`${classModOffers.join(` `)} places__list `}>{places}</div>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentOffer: id => dispatch(DataActions.setCurrentOffer(id))
-});
+OffersList.propTypes = {
+  classModPrefix: PropTypes.string.isRequired,
+  offers: PropTypes.array.isRequired,
+  classModOffers: PropTypes.array.isRequired,
+  mainClassMod: PropTypes.string.isRequired
+};
 
-export { OffersList };
-export default connect(null, mapDispatchToProps)(OffersList);
+export default OffersList;
